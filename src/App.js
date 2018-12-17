32f87f33251e39f733b07854b6554f52c0be543b
Amiya Behera <amiyatulu@gmail.com>
2018-12-17 07:49:40 +0530

    Understanding the base feature syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [ 
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age:26},
+      { name: 'Amiya', age:31}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [ 
+      { name: newName, age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age:26},
+      { name: 'Omy', age:12}
+    ]
+
+    })
+}
+
+ nameChangedHandler = (event) => {
+  this.setState({
+      persons: [ 
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: event.target.value, age:26},
+      { name: 'Amiya', age:31}
+    ]
+
+    })
+
+}
+
+
+
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor:'pointer'
+
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+       <div className="App">
+        <h1>Hi, I'm a React App!!!</h1>
+        <button style={style} onClick={this.switchNameHandler.bind(this,'NewMaximilian')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} 
+                age={this.state.persons[2].age} 
+                click={this.switchNameHandler.bind(this,'NewMax!!!')}
+                changed={this.nameChangedHandler}
+        >My Hobbies: Racing</Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+       </div>
     );
   }
 }
